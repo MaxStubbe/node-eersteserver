c22b276cb83699dff2624453b542c048af642fa3
MaxStubbe <maxyman@hotmail.nl>
2017-05-03 15:27:10 +0200

    grote verandering

--- server1.js
+++ server1.js
@@ -1,17 +1,16 @@
 var http = require('http');
 var express = require('express');
+var routes_v1 = require("./api/routes_v1")
+var routes_v2 = require("./api/routes_v2")
 
 var app = express();
 
+app.use('/api/v1', routes_v1);
+app.use('/api/v2', routes_v2);
 
-function onRequest(request, response){
-	console.log('er was een request');
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write('Super coole mega vette server');
-	response.end();
-}
 	
 app.listen(process.env.PORT || 3000, function(){
 	console.log('De server luistert op port 3000');
-};
+});
 	
+	module.exports = app;
